0b7cf3578d7fef1eb1f6b7f76807d14fc3e060a4
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-08 22:41:49 -0500

    feat: implement mail service and template for user password reset functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,25 @@ import { RoomsModule } from '../modules/rooms/rooms.module';
 import { ShowtimesModule } from '../modules/showtimes/showtimes.module';
 import { SeatsModule } from '../modules/seats/seats.module';
 import { TicketsModule } from '../modules/tickets/tickets.module';
+import { CommonModule } from '../common/common.module';
+import { MailsModule } from '../modules/mails/mails.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [UsersModule, MoviesModule, RoomsModule, ShowtimesModule, SeatsModule, TicketsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    MoviesModule,
+    RoomsModule,
+    ShowtimesModule,
+    SeatsModule,
+    TicketsModule,
+    CommonModule,
+    MailsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
