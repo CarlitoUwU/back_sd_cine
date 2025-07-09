c78246cbfdf340f6097bb0557fc854fc3e208c2a
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-08 22:41:36 -0500

    feat: add Redis module and service for password reset functionality

---
+++ src/common/common.module.ts
@@ -0,0 +1,9 @@
+import { Module, Global } from '@nestjs/common';
+import { RedisModule } from './redis/redis.module';
+
+@Global()
+@Module({
+  imports: [RedisModule],
+  exports: [RedisModule],
+})
+export class CommonModule {}
